1abf02c9e961777713b82c4b64e06ed788f2bab7
Galaad-CoudercVanhove <galaad-28@orange.fr>
2020-12-09 14:55:16 +0100

    Update galaad_couderc-vanhove_python.py

---
+++ galaad_couderc-vanhove_python.py
@@ -0,0 +1,19 @@
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+import random
+
+def regles():
+    print("Essayez de deviner le mot en 6 lettres")
+    print("Si une lettre apparaît en ROUGE, c'est qu'elle est bien placée")
+    print("Si une lettre apparaît en JAUNE, c'est qu'elle n'est pas bien placée")
+    print("Si une lettre apparaît en BLEU, c'est qu'elle n'est pas dans le mot")
+    print("Vous avez 8 chances pour trouver le bon mot")
+    print("Le mot peut être au pluriel")
+
+motsPossibles = ["wapiti","gloire","donjon","survie","joyeux","kayaks","humain","ocelot","oiseau"]
+
+def motAleatoire():
+    motSecret = r
+
+input()
