52be389107665259523fc4324ee083bd09acff73
Galaad-CoudercVanhove <galaad-28@orange.fr>
2020-12-09 15:37:31 +0100

    Update galaad_couderc-vanhove_python.py

--- galaad_couderc-vanhove_python.py
+++ galaad_couderc-vanhove_python.py
@@ -3,41 +3,43 @@ init()
 from colorama import Fore, Back, Style
 import random
 
-def regles():
+def regles(tour):
     print("Essayez de deviner le mot en 6 lettres")
     print("Si une lettre apparaît en ROUGE, c'est qu'elle est bien placée")
     print("Si une lettre apparaît en JAUNE, c'est qu'elle n'est pas bien placée")
     print("Si une lettre apparaît en BLEU, c'est qu'elle n'est pas dans le mot")
     print("Vous avez 8 chances pour trouver le bon mot")
     print("Le mot peut être au pluriel")
+    print("Tour n°",tour)
 
-regles()
+tour = 1
+regles(tour)
 motsPossibles = ["wapiti","gloire","donjon","survie","joyeux","kayaks","humain","ocelot","oiseau"]
 motAleatoire = random.choice(motsPossibles)
-tour = 0
 motJoueur = input("Ecrivez un mot en 6 lettres : ")
 
-def verification(motAleatoire):
+def verification(motJoueur):
     if (len(motJoueur) < 6):
         input("Ecrivez un mot en 6 lettres : ")
     if (len(motJoueur) < 6):
         input("Ecrivez un mot en 6 lettres : ")
-   
+verification(motJoueur)
+ 
 while (tour < 9):
     for i in range(0,6):
         if motJoueur[i] == motAleatoire[i]:
-            print(Back.RED + motJoueur,end="")
+            print(Back.RED + motJoueur[i],end="")
         if motJoueur[i] != motAleatoire[i]:
-            print(Back.BLUE + motJoueur,end="")
+            print(Back.BLUE + motJoueur[i],end="")
 
 
-
-
-def victoire:
+def victoire():
     if (motJoueur == motAleatoire):
         victoire = True
     else:
         tour = tour + 1
         print("Tour n°",tour)
         input("Ecrivez un autre mot en 6 lettres : ")
+victoire()
+
 input()
